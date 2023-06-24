f6790894605cb7a2f50449d205ef9d0518fac20b
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-23 23:33:09 -0700

    update

--- resources/views/components/dashboard-sidebar.blade.php
+++ resources/views/components/dashboard-sidebar.blade.php
@@ -19,7 +19,7 @@
                     <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i>
                         <span>Penjualan</span></a>
                     <ul class="dropdown-menu">
-                        <li><a class="nav-link" href="components-article.html">Article</a></li>
+                        <li><a class="nav-link" href="components-article.html">Data Penjualan</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
@@ -33,41 +33,21 @@
                     <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i>
                         <span>Peramalan</span></a>
                     <ul class="dropdown-menu">
-                        <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
-                        <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
-                        <li><a class="nav-link" href="forms-validation.html">Validation</a></li>
+                        <li><a class="nav-link" href="forms-advanced-form.html">Hasil Peramalan</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i>
                         <span>Kelola User</span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="gmaps-advanced-route.html">Advanced Route</a></li>
-                        <li><a href="gmaps-draggable-marker.html">Draggable Marker</a></li>
-                        <li><a href="gmaps-geocoding.html">Geocoding</a></li>
-                        <li><a href="gmaps-geolocation.html">Geolocation</a></li>
-                        <li><a href="gmaps-marker.html">Marker</a></li>
-                        <li><a href="gmaps-multiple-marker.html">Multiple Marker</a></li>
-                        <li><a href="gmaps-route.html">Route</a></li>
-                        <li><a href="gmaps-simple.html">Simple</a></li>
+                        <li><a href="gmaps-advanced-route.html">Daftar User</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i>
                         <span>Laporan</span></a>
                     <ul class="dropdown-menu">
-                        <li><a class="nav-link" href="modules-calendar.html">Calendar</a></li>
-                        <li><a class="nav-link" href="modules-chartjs.html">ChartJS</a></li>
-                        <li><a class="nav-link" href="modules-datatables.html">DataTables</a></li>
-                        <li><a class="nav-link" href="modules-flag.html">Flag</a></li>
-                        <li><a class="nav-link" href="modules-font-awesome.html">Font Awesome</a></li>
-                        <li><a class="nav-link" href="modules-ion-icons.html">Ion Icons</a></li>
-                        <li><a class="nav-link" href="modules-owl-carousel.html">Owl Carousel</a></li>
-                        <li><a class="nav-link" href="modules-sparkline.html">Sparkline</a></li>
-                        <li><a class="nav-link" href="modules-sweet-alert.html">Sweet Alert</a></li>
-                        <li><a class="nav-link" href="modules-toastr.html">Toastr</a></li>
-                        <li><a class="nav-link" href="modules-vector-map.html">Vector Map</a></li>
-                        <li><a class="nav-link" href="modules-weather-icon.html">Weather Icon</a></li>
+                        <li><a class="nav-link" href="modules-calendar.html">Laporan Penjualan</a></li>
                     </ul>
                 </li>
 
