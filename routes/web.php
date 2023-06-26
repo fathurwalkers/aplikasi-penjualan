aa4f05cb4000ced8e7024c2b88bf4516353888ae
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-25 23:23:22 -0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,35 @@
 <?php
 
-use App\Http\Controllers\BackController;
-use App\Http\Controllers\LaporanController;
-use App\Http\Controllers\ProdukController;
-use App\Http\Controllers\PenjualanController;
-use App\Http\Controllers\PeramalanController;
+use App\Http\Controllers\{BackController, LaporanController, ProdukController, PenjualanController, PeramalanController};
+// use App\Http\Controllers\LaporanController;
+// use App\Http\Controllers\ProdukController;
+// use App\Http\Controllers\PenjualanController;
+// use App\Http\Controllers\PeramalanController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/login', [BackController::class, 'login'])->name('login');
 Route::post('/login/post-login', [BackController::class, 'postlogin'])->name('post-login');
 Route::post('/logout', [BackController::class, 'logout'])->name('logout');
 
-Route::get('/', fn () => redirect()->route('dashboard'));
+// Route::get('/', fn () => redirect()->route('dashboard'));
+Route::get('/', function () {
+    $string = "hayatsipalingenskripsi";
+    $string2 = "hayatsipalingenskripsi";
+
+    $hashString1 = Hash::make($string, [
+        'rounds' => 12,
+    ]);
+    $hashString2 = Hash::make($string2, [
+        'rounds' => 12,
+    ]);
+
+    dump($string);
+    dump($string2);
+    dump($hashString1);
+    dump($hashString2);
+
+});
 
 Route::group(['prefix' => '/dashboard', 'middleware' => 'ceklogin'], function () {
     Route::get('/', [BackController::class, 'index'])->name('dashboard');
