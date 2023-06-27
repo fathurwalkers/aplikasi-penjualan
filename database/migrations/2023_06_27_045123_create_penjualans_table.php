45feac20816ca39877b500202d6f32bf6f8ea772
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-26 21:53:48 -0700

    update

---
+++ database/migrations/2023_06_27_045123_create_penjualans_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('penjualan', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('barang_nama')->nullable();
+            $table->string('barang_kategori')->nullable();
+            $table->string('barang_ukuran')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('penjualan');
+    }
+};
