7e82797f37ca30aae1f44db7ee1dd2fa3473ec53
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-26 23:03:38 -0700

    update

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -9,17 +9,6 @@ class ProdukController extends Controller
 {
     public function daftar_produk($kategori)
     {
-        // switch ($kategori) {
-        //     case 'SD':
-        //         $barang = Barang::where('barang_kategori', 'SD')->get();
-        //         break;
-        //     case 'SMP':
-        //         $barang = Barang::where('barang_kategori', 'SMP')->get();
-        //         break;
-        //     case 'SMA':
-        //         $barang = Barang::where('barang_kategori', 'SMA')->get();
-        //         break;
-        // }
         $barang = Barang::where('barang_kategori', $kategori)->get();
         return view('dashboard.produk.daftar-produk', [
             'barang' => $barang,
