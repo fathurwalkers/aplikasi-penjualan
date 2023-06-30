061297b54e7448b9dac98ec36a4bcf2bdff3e2e5
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-30 13:01:22 -0700

    menambahkan item Produk Kode pada view Daftar Produk

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\{Request, Str};
+use Illuminate\Http\Request;
 use App\Models\{Barang, Login};
+use Illuminate\Support\Str;
 
 class ProdukController extends Controller
 {
