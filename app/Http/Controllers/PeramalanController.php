d6758fed3ef943c8680387ff7086b471e455d871
Fathur Walkers <fathurwalkers44@gmail.com>
2023-07-16 10:19:52 -0700

    update

--- app/Http/Controllers/PeramalanController.php
+++ app/Http/Controllers/PeramalanController.php
@@ -26,57 +26,77 @@ class PeramalanController extends Controller
         $tahun = $request->tahun;
         $periode = $request->periode;
 
+        // $penjualan = $penjualan = Penjualan::where('penjualan_tahun', $tahun)->whereMonth('penjualan_bulan_awal', '>=', $bulan_awal)->whereMonth('penjualan_bulan_akhir', '<=', $bulan_akhir)->whereHas('barang', function ($query) use ($barang_id) {
+        //     $query->where('id', $barang_id);
+        // })->get();
+
+        // $totalData = count($penjualan);
+        // $hasilMovingAverage = [];
+
+        // for ($i = 0; $i < $totalData; $i++) {
+        //     $start = max(0, $i - $periode + 1);
+        //     $end = $i + 1;
+        //     $jumlahData = $end - $start;
+
+        //     $totalJumlah = $penjualan->slice($start, $jumlahData)->sum('penjualan_jumlah');
+        //     $movingAverage = $totalJumlah / $jumlahData;
+
+        //     $hasilMovingAverage[$i] = $movingAverage;
+        // }
+
+        // $totalMAPE = 0;
+        // $totalDataPenjualan = $totalData;
+
+        // for ($i = $periode; $i < $totalDataPenjualan; $i++) {
+        //     $nilaiAktual = $penjualan[$i]->penjualan_jumlah;
+        //     $nilaiRamalan = $hasilMovingAverage[$i];
+
+        //     $mape = abs(($nilaiAktual - $nilaiRamalan) / $nilaiAktual) * 100;
+        //     $totalMAPE += $mape;
+        // }
+
+        // =========================================================================================================
+        // =========================================================================================================
+
         $penjualan = $penjualan = Penjualan::where('penjualan_tahun', $tahun)->whereMonth('penjualan_bulan_awal', '>=', $bulan_awal)->whereMonth('penjualan_bulan_akhir', '<=', $bulan_akhir)->whereHas('barang', function ($query) use ($barang_id) {
             $query->where('id', $barang_id);
-        })->get();
+        })->get()->toArray();
 
         $totalData = count($penjualan);
-        $hasilMovingAverage = [];
 
-        for ($i = 0; $i < $totalData; $i++) {
-            $start = max(0, $i - $periode + 1);
-            $end = $i + 1;
-            $jumlahData = $end - $start;
+        for ($i=0; $i < $totalData; $i++) {
+            // // Define the sales data
+            $sales = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-            $totalJumlah = $penjualan->slice($start, $jumlahData)->sum('penjualan_jumlah');
-            $movingAverage = $totalJumlah / $jumlahData;
+            // // Define the number of periods to forecast
+            $periods = 3;
 
-            $hasilMovingAverage[$i] = $movingAverage;
+            // // Calculate the SMA
+            $sma = collect($sales)->take(-$periods)->average();
+
+            // // Define the actual sales data for the forecast period
+            $actual = $penjualan[$i]['penjualan_jumlah'];
+
+            // // Calculate the forecast sales data for the forecast period
+            $forecast = array_fill(0, $periods, $sma);
+
+            // Calculate the MAPE
+            $mape = collect($actual)->zip($forecast)->map(function ($item) {
+                if ($item[0] != 0) {
+                    return abs(($item[0] - $item[1]) / $item[0]);
+                } else {
+                    return 0; // or handle the case when the divisor is zero
+                }
+            })->average();
+
+            dump($sales);
+            dump($periods);
+            dump($sma);
+            dump($forecast);
+            dump($mape);
+            dd($actual);
+
         }
-
-        $totalMAPE = 0;
-        $totalDataPenjualan = $totalData;
-
-        for ($i = $periode; $i < $totalDataPenjualan; $i++) {
-            $nilaiAktual = $penjualan[$i]->penjualan_jumlah;
-            $nilaiRamalan = $hasilMovingAverage[$i];
-
-            $mape = abs(($nilaiAktual - $nilaiRamalan) / $nilaiAktual) * 100;
-            $totalMAPE += $mape;
-        }
-
-        // =========================================================================================================
-        // =========================================================================================================
-
-        // // Define the sales data
-        // $sales = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-
-        // // Define the number of periods to forecast
-        // $periods = 3;
-
-        // // Calculate the SMA
-        // $sma = collect($sales)->take(-$periods)->average();
-
-        // // Define the actual sales data for the forecast period
-        // $actual = [110, 120, 130];
-
-        // // Calculate the forecast sales data for the forecast period
-        // $forecast = array_fill(0, $periods, $sma);
-
-        // // Calculate the MAPE
-        // $mape = collect($actual)->zip($forecast)->map(function ($item) {
-        //     return abs(($item[0] - $item[1]) / $item[0]);
-        // })->average() * 100;
 
         // // Output the results
         // echo "SMA: " . $sma . "\n";
