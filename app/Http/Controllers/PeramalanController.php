831cdfe7983a6cd290d25abfa7f454e7f8b159ef
Fathur Walkers <fathurwalkers44@gmail.com>
2023-07-16 12:59:32 -0700

    update

--- app/Http/Controllers/PeramalanController.php
+++ app/Http/Controllers/PeramalanController.php
@@ -24,10 +24,66 @@ class PeramalanController extends Controller
         $bulan_awal = $request->bulan_awal;
         $bulan_akhir = $request->bulan_akhir;
         $barang_id = intval($request->id_produk);
-        // $tahun = $request->tahun;
         $tahun = '2023';
         $periode = $request->periode;
 
+        $penjualan = Penjualan::where('penjualan_tahun', $tahun)->whereMonth('penjualan_bulan_awal', '>=', $bulan_awal)->whereMonth('penjualan_bulan_akhir', '<=', $bulan_akhir)->whereHas('barang', function ($query) use ($barang_id) {
+            $query->where('id', $barang_id);
+        })->get();
+
+        $array_jumlah_penjualan = [];
+        $hasilMovingAverage = [];
+
+        foreach ($penjualan as $itemsss) {
+            $array_jumlah_penjualan = Arr::prepend($array_jumlah_penjualan, $itemsss->penjualan_jumlah);
+        }
+
+        $totalData = count($penjualan);
+        $totalMAPE = 0;
+        $totalDataPenjualan = $totalData;
+
+        for ($i=0; $i < $totalData; $i++) {
+            $sales = $array_jumlah_penjualan;
+            $periods = intval($request->periode);
+            $sma = collect($sales)->take(-$periods)->average();
+            $actual = $penjualan[$i]->penjualan_jumlah;
+            $forecast = array_fill(0, $periods, $sma);
+            $mape = collect($actual)->zip($forecast)->map(function ($item) {
+                if ($item[0] != 0) {
+                    return abs(($item[0] - $item[1]) / $item[0]);
+                } else {
+                    return 0;
+                }
+            })->average();
+            $totalMAPE += $mape;
+            $hasilMovingAverage[$i] = $mape;
+        }
+
+        $maper = $totalMAPE;
+        $session_hasilmovingaverage = session(['hasilMovingAverage' => $hasilMovingAverage]);
+        $session_penjualan = session(['penjualan' => $penjualan]);
+        $session_maper = session(['maper' => $maper]);
+        return view('dashboard.peramalan.hasil-peramalan', [
+            'hasilMovingAverage' => $hasilMovingAverage,
+            'penjualan' => $penjualan,
+            'maper' => $maper
+        ]);
+    }
+
+    public function print_peramalan_penjualan()
+    {
+        $penjualan = session('penjualan');
+        $hasilMovingAverage = session('hasilMovingAverage');
+        $maper = session('maper');
+        return view('dashboard.peramalan.print-laporan-peramalan', [
+            'penjualan' => $penjualan,
+            'hasilMovingAverage' => $hasilMovingAverage,
+            'maper' => $maper,
+        ]);
+    }
+
+    public function trash_function()
+    {
         // $penjualan = Penjualan::where('penjualan_tahun', $tahun)->whereMonth('penjualan_bulan_awal', '>=', $bulan_awal)->whereMonth('penjualan_bulan_akhir', '<=', $bulan_akhir)->whereHas('barang', function ($query) use ($barang_id) {
         //     $query->where('id', $barang_id);
         // })->get()->toArray();
@@ -63,89 +119,5 @@ class PeramalanController extends Controller
 
         // =========================================================================================================
         // =========================================================================================================
-
-        $penjualan = Penjualan::where('penjualan_tahun', $tahun)->whereMonth('penjualan_bulan_awal', '>=', $bulan_awal)->whereMonth('penjualan_bulan_akhir', '<=', $bulan_akhir)->whereHas('barang', function ($query) use ($barang_id) {
-            $query->where('id', $barang_id);
-        })->get();
-
-        $array_jumlah_penjualan = [];
-        $hasilMovingAverage = [];
-
-        foreach ($penjualan as $itemsss) {
-            $array_jumlah_penjualan = Arr::prepend($array_jumlah_penjualan, $itemsss->penjualan_jumlah);
-        }
-
-        $totalData = count($penjualan);
-        $totalMAPE = 0;
-        $totalDataPenjualan = $totalData;
-
-        for ($i=0; $i < $totalData; $i++) {
-            // // Define the sales data
-            $sales = $array_jumlah_penjualan;
-
-            // // Define the number of periods to forecast
-            $periods = intval($request->periode);
-
-            // // Kalkulasi satuan produk
-            $sma = collect($sales)->take(-$periods)->average();
-
-            // // Define the actual sales data for the forecast period
-            $actual = $penjualan[$i]->penjualan_jumlah;
-
-            // // Calculate the forecast sales data for the forecast period
-            $forecast = array_fill(0, $periods, $sma);
-
-            // Calculate the MAPE
-            $mape = collect($actual)->zip($forecast)->map(function ($item) {
-                if ($item[0] != 0) {
-                    return abs(($item[0] - $item[1]) / $item[0]);
-                } else {
-                    return 0; // or handle the case when the divisor is zero
-                }
-            })->average();
-            $totalMAPE += $mape;
-            $hasilMovingAverage[$i] = $mape;
-
-            // dump($sales);
-            // dump($periods);
-            // dump($sma);
-            // dump($forecast);
-            // dump($mape);
-            // dd($actual);
-
-        }
-
-        // dump($totalMAPE);
-        // dump($hasilMovingAverage);
-        // // Output the results
-        // echo "SMA: " . $sma . "\n";
-        // echo "Forecast: " . implode(", ", $forecast) . "\n";
-        // echo "Actual: " . implode(", ", $actual) . "\n";
-        // echo "MAPE: " . $mape . "%\n";
-
-        // =========================================================================================================
-        // =========================================================================================================
-        $maper = $totalMAPE;
-        // dd($maper);
-        $session_hasilmovingaverage = session(['hasilMovingAverage' => $hasilMovingAverage]);
-        $session_penjualan = session(['penjualan' => $penjualan]);
-        $session_maper = session(['maper' => $maper]);
-        return view('dashboard.peramalan.hasil-peramalan', [
-            'hasilMovingAverage' => $hasilMovingAverage,
-            'penjualan' => $penjualan,
-            'maper' => $maper
-        ]);
-    }
-
-    public function print_peramalan_penjualan()
-    {
-        $penjualan = session('penjualan');
-        $hasilMovingAverage = session('hasilMovingAverage');
-        $maper = session('maper');
-        return view('dashboard.peramalan.print-laporan-peramalan', [
-            'penjualan' => $penjualan,
-            'hasilMovingAverage' => $hasilMovingAverage,
-            'maper' => $maper,
-        ]);
     }
 }
